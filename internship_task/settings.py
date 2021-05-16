56947bd945e44abfde8d6e64dd27d379a4e136ae
neha <rajbhandaripratik8@gmail.com>
2021-05-16 15:20:30 +0545

    initial template setup

--- internship_task/settings.py
+++ internship_task/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -51,11 +51,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'internship_task.urls'
-
+template_dirs = os.path.join(BASE_DIR, "templates")
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [template_dirs],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
